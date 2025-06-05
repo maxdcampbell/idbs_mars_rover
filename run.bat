2f80674e10477280f7a5405e096e8a737c301463
Darren Jones <DJones@idbs.com>
2025-06-05 12:38:05 +0100

    Attempt to fix run script

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
+call git clone https://github.com/maxdcampbell/idbs_mars_rover
+cd idbs_mars_rover
 call mvn package
 java -cp target/idbs_mars_rover-1.0-SNAPSHOT.jar com.codemanship.marsrover.Program
