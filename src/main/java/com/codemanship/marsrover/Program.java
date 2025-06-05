b2085bd85e06bd6cbd9ca2466a8d96bb4b5eb735
mcampbellIDBS <110536474+mcampbellIDBS@users.noreply.github.com>
2025-06-05 12:48:58 +0100

    Add input handling for initialisation

--- src/main/java/com/codemanship/marsrover/Program.java
+++ src/main/java/com/codemanship/marsrover/Program.java
@@ -1,7 +1,17 @@
 package com.codemanship.marsrover;
 
+import java.util.Scanner;
+
 public class Program {
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner myObj = new Scanner(System.in);
+        MarsRover rover = new MarsRover();
+
+        System.out.println("Enter target coordinates and direction:");
+        String initilisationString = myObj.nextLine();
+        String confirmation = rover.initialise(initilisationString);
+        System.out.println(confirmation);
     }
+
 }
